f044cd0d13cd365cc85853073ccbf7c9bceb97b3
Maximiliano Reyes <maximilianoreyes@iMac-de-Maximiliano.local>
2020-08-27 11:37:48 -0300

    #1 - Base code and routes initial

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = () => {
+  router.get("/", (req, res) => {
+    res.send("inicio");
+  });
+
+  router.get("/nosotros", (req, res) => {
+    res.send("nosotros");
+  });
+
+  return router;
+};
